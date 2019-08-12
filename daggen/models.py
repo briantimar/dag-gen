2298674d9797d03e1d34986e4dc4ec763819aa78
Brian Timar <timarbrian@gmail.com>
2019-08-12 15:32:25 -0400

    sample_graph_tensors

--- daggen/models.py
+++ daggen/models.py
@@ -1,6 +1,7 @@
 """ Models for parameterizing distributions over graphs. """
 
 import torch
+from torch.distributions.categorical import Categorical
 
 class MLP(torch.nn.Module):
     """Dense network."""
@@ -23,42 +24,175 @@ class MLP(torch.nn.Module):
                 x = self.activation(x)
         return x
 
+class TwoLayerMLP(MLP):
+    """ Two linear applications, ie a single hidden layer. """
 
+    def __init__(self, input_size, hidden_size, output_size, activation=torch.nn.ReLU):
+        self.hidden_size = hidden_size
+        self.input_size = input_size
+        self.output_size = output_size
+        super().__init__([input_size, hidden_size, output_size], activation=activation)
 
 class GraphRNN(torch.nn.Module):
     """An autoregressive graph model a la https://arxiv.org/abs/1802.08773."""
 
     def __init__(self, vertex_cell, edge_cell, activation_cell, 
-                    edge_logits, activation_logits):
+                    vertex_logits, edge_logits, activation_logits):
         """ `vertex_cell`: the `backbone` of the graph generator. Applied once for each vertex in the graph. Takes as input vertex hidden state and edge hidden state
         from the previous vertex, outputs new hidden state.
             `edge_cell`: computes probabilities for a particular vertex to be connected to its predecessors. Takes as input edge hidden state, and binary adjacency value from a 
             previous vertex; computes new edge hidden state. 
             `activation_cell`: takes as input edge hidden state, and a binary adjacency value; computes new edge hidden state.
+            `vertex_logits`: takes as input vertex hidden state, returns two logits indicating whether a new vertex should be added (not adding a vertex corresponds to 'end of sequence')
             `edge_logits`: takes as input edge hidden state, returns logits for determining connectivity (two classes, connected or unconnected).
             `activation_logits`: takes as input edge hidden state, returns logits for determining activation function to be applied"""
         
         super().__init__()
-        # self.vertex_cell = vertex_cell
-        # self.edge_cell = edge_cell
-        # self.activation_cell = activation_cell
-        # self.edge_logits = edge_logits
-        # self.activation_logits = activation_logits
+
         self.add_module('vertex_cell', vertex_cell)
         self.add_module('edge_cell', edge_cell)
         self.add_module('activation_cell', activation_cell)
+        self.add_module('vertex_logits', vertex_logits)
         self.add_module('edge_logits', edge_logits)
         self.add_module('activation_logits', activation_logits)
+
 
 
 class GraphGRU(GraphRNN):
     """ Graph generator which uses GRU cells."""
 
-    def __init__(self, hidden_size):
-        vertex_cell = torch.nn.GRUCell(hidden_size, hidden_size)
+    def __init__(self, hidden_size, logits_hidden_size, num_activations):
+        """ hidden_size: dimensionality of the hidden state for vertex and edge cells.
+            logits_hidden_size: size of the hidden layer used in computing logits from hidden state
+            num_activations: the number of possible activation functions. """
+
+        self.hidden_size = hidden_size
+        self.logits_hidden_size = logits_hidden_size
+        self.num_activations = num_activations
+        self.vertex_input_size = hidden_size + 1
+
+        vertex_cell = torch.nn.GRUCell(self.vertex_input_size, hidden_size)
         edge_cell = torch.nn.GRUCell(1, hidden_size)
         activation_cell = torch.nn.GRUCell(1, hidden_size)
-        edge_logits = torch.nn.Lin
+        vertex_logits = TwoLayerMLP(hidden_size, logits_hidden_size, 2)
+        edge_logits = TwoLayerMLP(hidden_size, logits_hidden_size, 2)
+        activation_logits = TwoLayerMLP(hidden_size, logits_hidden_size, num_activations)
 
-    
+        super().__init__(vertex_cell, edge_cell, activation_cell, 
+                        vertex_logits, edge_logits, activation_logits)
 
+        ## initialization of the hidden state
+        self.hidden_init = torch.nn.Parameter(torch.randn(hidden_size))
+        self.vertex_input_init = torch.nn.Parameter(torch.randn(self.vertex_input_size))
+
+    def _get_samples_and_log_probs(self, logits):
+        """ Obtain samples and log-probabilities, using a Categorical distribution, for the given logits."""
+        dist = Categorical(logits=logits)
+        samples = dist.sample()
+        lps = dist.log_prob(samples)
+        return samples, lps
+
+    def sample_graph_tensors(self, N, max_vertices=100 ):
+        """ Sample N graph encodings from the GraphGRU.
+        
+        `N`: how many graphs to sample
+        `max_vertices`: if not None, the max number of vertices to permit in each graph.
+        
+        Returns: next_active_vertices (N, maxnum) bool tensor specifying which vertices exist in each graph.
+                connections_all (N, maxnum, maxnum) bool tensor. The i, j, k element is nonzero iff a connection k -> j exists in the ith graph.
+                activations (N, maxnum) int tensor specifying an activation function to be applied at each vertex."""
+
+        vertex_index = 0
+        graph_complete = False
+        vertex_hidden_state = self.hidden_init.view(1, -1).expand(N, self.hidden_size)
+        vertex_input = self.vertex_input_init.view(1, -1).expand(N, self.vertex_input_size)
+
+        log_probs = []
+
+        next_active_vertices = []
+        all_connections = []
+        activations = []
+
+        #keep track of which graphs are still being sampled
+        next_active_graphs = torch.ones(N,dtype=torch.uint8)
+        
+        while not graph_complete:
+            #compute new hidden state
+            vertex_hidden_state = self.vertex_cell(vertex_input, vertex_hidden_state)
+            
+            #check whether another vertex should be added
+            vertex_logits = self.vertex_logits(vertex_hidden_state)
+            add_vertex, lp_vertex = self._get_samples_and_log_probs(vertex_logits)
+            #a graph is active only if it has added a vertex at each sampling step.
+            # these are the graphs for which another vertex will be added after connections have been determined for the current vertex
+            next_active_graphs = next_active_graphs & (add_vertex > 0)
+
+            next_active_vertices.append(next_active_graphs.data)
+
+            #now pass hidden state down to edge cells
+            edge_hidden_state = vertex_hidden_state
+            edge_input_init = torch.zeros(N,1)
+            edge_input = edge_input_init
+
+            #determine whether to connect to each previous vertex
+            connections = []
+            for prev_index in range(vertex_index):
+                #compute edge hidden state
+                edge_hidden_state = self.edge_cell(edge_input, edge_hidden_state)            
+                #sample connectivity to prev_index
+                connection_logits = self.edge_logits(edge_hidden_state)
+                add_connection, lp_connection = self._get_samples_and_log_probs(connection_logits)
+                lp_vertex += lp_connection
+                connections.append(add_connection)
+
+                edge_input = add_connection.view(N, 1).to(dtype=torch.float)
+            
+            all_connections.append(connections)
+            # finally, determine which activation function to apply to this vertex
+            act_state = self.activation_cell(edge_input, edge_hidden_state)
+            act_logits = self.activation_logits(act_state)
+            activation, lp_act = self._get_samples_and_log_probs(act_logits)
+            lp_vertex += lp_act
+
+            activations.append(activation)
+            log_probs.append(lp_vertex)
+
+            #compute input to the next vertex cell
+            vertex_input = torch.cat((edge_hidden_state, activation.view(-1, 1).to(dtype=torch.float)), dim=1)
+            
+            vertex_index += 1
+            ##sampling halts when all graph sequences have terminated, or max number of vertices is reached
+            graph_complete = next_active_graphs.sum().item()==0 or (max_vertices is not None and vertex_index == max_vertices)
+
+        #when all graphs have finished sampling, stack together the sequences that define the graphs, and the corresponding log-probs
+
+        #(N, max_num_vertices) tensor specifying the selected activations
+        #NOTE this gets stacked along dim 1 because the each entry in the activations list corresponds to a single vertex (and has length N)
+        activations = torch.stack(activations,dim=1)
+
+        # (N, max_num_vertices) tensor specifying whether a vertex will be added at each step
+        # once a vertex is not added, the graph is dead
+        next_active_vertices = torch.stack(next_active_vertices, dim=1)      
+        #same thing, but now indicating whether a graph is active at step i
+        active_vertices = torch.ones_like(next_active_vertices)
+        active_vertices[:, 1:] = next_active_vertices[:, :-1]
+        
+        #number of vertices in the largest graph of the batch
+        max_num_vertices = activations.size(1)
+
+        #bool tensor which defines the connectivity of each graph
+        #the i, j, k entry indicates whether a connection k ->j exists in the ith graph
+        connections_all = torch.zeros(N, max_num_vertices, max_num_vertices, dtype=torch.long)
+        
+        #log-probabilities for each graph
+        log_probs = torch.stack(log_probs, dim=1)
+        # graph sampling stops when first "end of sequence" is reached.
+        log_probs[~active_vertices] = 0
+
+        for i in range(1,max_num_vertices):
+            # for each graph, lookup which connections flow into the ith neuron
+            #(N, i) tensor indicating connections to previous tensors
+            conns = torch.stack(all_connections[i], dim=1)
+            connections_all[:, i, :i] = conns
+        
+        return next_active_vertices, connections_all, activations, log_probs.sum(dim=1)
--- daggen/test.py
+++ daggen/test.py
@@ -1,5 +1,6 @@
 import unittest
 import torch
+import numpy as np
 
 class TestMLP(unittest.TestCase):
 
@@ -21,6 +22,74 @@ class TestMLP(unittest.TestCase):
         self.assertEqual(tuple(y1.shape), (13, 2))
         self.assertEqual(tuple(y2.shape), (13, 2))
 
+class TestTwoLayerMLP(unittest.TestCase):
+
+    def test_sizes(self):
+        from .models import TwoLayerMLP
+        mlp = TwoLayerMLP(5, 4, 2)
+        self.assertEqual(mlp.num_layers, 2)
+
+class TestGraphGRU(unittest.TestCase):
+
+    def setUp(self):
+        from .models import GraphGRU
+        hidden_size=16
+        logits_hidden_size=4
+        num_activations = 5
+        self.test_graph_gru = GraphGRU(hidden_size, logits_hidden_size, num_activations)
+        
+    def test_build(self):
+        from .models import GraphGRU
+        hidden_size=16
+        logits_hidden_size=4
+        num_activations = 5
+        gg = GraphGRU(hidden_size, logits_hidden_size, num_activations)
+
+    def test_modules(self):
+        for module in ['vertex_cell', 'edge_cell', 'activation_cell', 'vertex_logits', 'edge_logits', 'activation_logits']:
+            self.assertTrue(hasattr(self.test_graph_gru, module))
+
+    def test_params(self):
+        params = list(self.test_graph_gru.named_parameters(recurse=False))
+        self.assertTrue(len(params)==2)
+        param_names = [p[0] for p in params]
+        self.assertTrue('hidden_init' in param_names)
+        self.assertTrue('vertex_input_init' in param_names)
+
+    def test__get_samples_and_log_probs(self):
+        l1 = torch.randn(10, 4)
+        
+        s, lps = self.test_graph_gru._get_samples_and_log_probs(l1)
+        self.assertEqual(tuple(s.shape), (10,))
+        self.assertTrue(max(s) < 4)
+        
+    def test_sample_graph_tensors(self):
+        batch_size=5
+        max_vertices=None
+        next_active_vertices, connections, activations, log_probs = self.test_graph_gru.sample_graph_tensors(batch_size, max_vertices=max_vertices)
+        
+        #check that all outputs have the right shape
+        max_vertices = next_active_vertices.size(1)
+        for t in (next_active_vertices, activations):
+            self.assertEqual(tuple(t.shape), (batch_size, max_vertices))
+        self.assertEqual(tuple(connections.shape), (batch_size, max_vertices, max_vertices))
+        self.assertEqual(tuple(log_probs.shape), (batch_size,))
+
+        #active_vertices indicates whether another vertex ought to be added. At the final step this should be false for all graphs
+        self.assertEqual(next_active_vertices[:, -1].sum().item(), 0)
+        #also, a graph should never go from being inactive to active
+        react = np.diff(next_active_vertices.to(dtype=torch.long).numpy(), axis=1) > 0
+        self.assertEqual(np.sum(react), 0)
+
+        #no vertex should have connections flowing into it from future vertices
+        for i in range(max_vertices):
+            self.assertEqual(connections[:, i, i:].sum().item(), 0)
+
+        #activations entries should lie within the expected range
+        self.assertTrue( 0 <= activations.min().item() and activations.max().item() < self.test_graph_gru.num_activations )
+
+        #log probs are nonpositive
+        self.assertEqual( (log_probs <=0).sum().item(), batch_size )
 
 if __name__ == "__main__":
     unittest.main()
