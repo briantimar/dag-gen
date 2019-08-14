9d69eab75013f3fe7e85ea98762adcc44b643e4a
Brian Timar <timarbrian@gmail.com>
2019-08-14 17:56:28 -0400

    is_valid_adjacency_matrix

--- daggen/test_utils.py
+++ daggen/test_utils.py
@@ -1,4 +1,30 @@
 import unittest
+import torch
+
+class TestDAGChecks(unittest.TestCase):
+    """Check whether DAG-testing functions work as expected."""
+
+    def test_is_valid_adjacency_matrix(self):
+        """ Check whether adjacency tensors are correctly identified."""
+        from .utils import is_valid_adjacency_matrix
+        input_dim = 2
+        output_dim = 1
+        num_intermediate = 1
+        #ok
+        conn1 = torch.tensor([[1, 1, 0], [1, 1, 1]], dtype=torch.uint8)
+        #too small
+        conn2 = torch.zeros(output_dim, input_dim, dtype=torch.uint8)
+        #inconsistent shape
+        conn3 = torch.zeros(output_dim + 3, input_dim + 2, dtype = torch.uint8)
+        # has non-ancestral connections
+        conn4 = torch.tensor([[1, 0, 1], [1, 1, 1]], dtype=torch.uint8)
+        # is not left-justified (embedded in dag space with 2 intermediate)
+        conn5 = torch.tensor([[1, 0, 0, 0], [0, 0, 0, 0], [1, 0, 0, 0], [1, 0, 0, 0]], dtype=torch.uint8)
+        
+        self.assertTrue(is_valid_adjacency_matrix(conn1, num_intermediate, input_dim, output_dim))
+        for conn in (conn2, conn3, conn4, conn5):
+            self.assertFalse(is_valid_adjacency_matrix(conn, num_intermediate, input_dim, output_dim))
 
 if __name__ == "__main__":
     unittest.main()
+
--- daggen/utils.py
+++ daggen/utils.py
@@ -1,3 +1,48 @@
 import graphviz
 
-def build_graphviz()
+def is_valid_adjacency_matrix(connections, num_intermediate, num_input, num_output):
+    """ Check whether input defines a valid, left-justified adjacency matrix.
+    `connections`: (max_num_receiving, max_num_emitting) uint8 tensor
+    `num_intermediate`: int, number of intermediate vertices
+    `num_input`: int, number of input vertices
+    `num_output`: int, number of output vertices"""
+
+    num_emitting = num_intermediate + num_input
+    num_receiving = num_intermediate + num_output
+
+    if connections.size(0) < num_receiving:
+        return False
+    if connections.size(1) < num_emitting:
+        return False
+
+    embedded_intermediate_size = connections.size(0) - num_output
+    #check that dimensions of the connectivity tensor are consistent with single fixed intermediate size
+    if embedded_intermediate_size < 0 or embedded_intermediate_size != connections.size(1) - num_input:
+        return False
+
+    # check left-justified
+    if connections[num_receiving:, :].sum().item() > 0:
+        return False
+    if connections[:, num_emitting:].sum().item() > 0:
+        return False
+    # check that vertices only receive input from ancestors
+    for i in range(num_receiving):
+        if connections[i, i+ num_input:].sum().item() > 0:
+            return False
+    return True
+
+
+def build_graphviz(input_dim, output_dim, num_intermediate, 
+                connections, activations, activation_labels):
+    """ Build graphviz object corresponding to the single DAG specified by the input tensors.
+        `input_dim`: int, number of input vertices
+        `output_dim`: int, number of output vertices
+        `num_intermediate`: int, number of intermediate vertices
+        `connections`: (max_num_receiving, max_num_emitting) uint8 adjacency matrix
+        `activations`: (max_num_receiving,) int tensor specifying activations to apply at each
+        receiving vertex
+        `activation_labels`: list of activation function labels. 
+        
+        Returns: graphviz digraph"""
+    
+    pass
