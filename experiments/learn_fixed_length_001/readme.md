81fadd35bfbb2e3f86b33a94dd29aac30df8cf02
Brian Timar <timarbrian@gmail.com>
2019-08-20 14:31:15 -0400

    size-learning experiment

--- experiments/learn_fixed_length_001/readme.md
+++ experiments/learn_fixed_length_001/readme.md
@@ -2,3 +2,12 @@
 
 A check that the daggen code behaves as expected: teach the graphrnn to output graphs of a fixed length.
 Training is via "policy gradient".
+
+### Result
+As one would hope this is not too hard to train.
+Here's an example of training (sacred experiment id 683) which plots the cost function (minus the difference between specified and sampled sizes) and
+batch graph size as a function of update step.
+
+![tr](tr_example.png)
+
+I used a running-average baseline, hidden size 10. Not much more to say here -- backprop seems to work. 
--- experiments/learn_fixed_length_001/train.py
+++ experiments/learn_fixed_length_001/train.py
@@ -5,7 +5,7 @@ import numpy as np
 import torch
 
 learn_length = Experiment(name="learn_fixed_length_001")
-# learn_length.observers.append(MongoObserver.create())
+learn_length.observers.append(MongoObserver.create())
 
 @learn_length.config
 def model_config():
@@ -29,16 +29,18 @@ def training_config():
     # desired number of intermediate vertices
     target_num_intermediate = 5
     # how many graphs to sample per training step
-    batch_size = 5
+    batch_size = 10
     # how many samples to draw from model
-    total_samples = 400
+    total_samples = 1000
     #learning rate
     learning_rate = .01
+    #number of updates between logging events
+    log_every = 1
 
 @learn_length.automain
 def train(hidden_size, input_dim, output_dim, logits_hidden_size, num_activations, 
             min_intermediate_vertices, max_intermediate_vertices,
-        target_num_intermediate, batch_size, total_samples, learning_rate
+        target_num_intermediate, batch_size, total_samples, learning_rate, log_every
         ):
     """ Train a graphgru to only output graphs of a given size."""
     #build the model
@@ -47,11 +49,21 @@ def train(hidden_size, input_dim, output_dim, logits_hidden_size, num_activation
     dag_model.activation_functions = [lambda x: x]
     ## define score function for a given graph
     def score(graph):
-        return np.abs(graph.get_num_intermediate() - target_num_intermediate, dtype=float)
+        return - np.abs(graph.get_num_intermediate() - target_num_intermediate, dtype=float)
     
     optimizer = torch.optim.Adam(dag_model.parameters(), lr=learning_rate)
-    score_logger = lambda s: print(f"Batch score {s}")
+    def score_logger(s):
+        print(f"Batch score {s}")
+        learn_length.log_scalar("score", s)
+    def get_mean_size(dags):
+        size =  np.mean([dag.size for dag in dags])
+        learn_length.log_scalar("size", size)
+    
+    network_callbacks = [get_mean_size]
+
     do_score_training(dag_model, score, total_samples, batch_size, optimizer, 
                             min_intermediate_vertices=min_intermediate_vertices, 
                             max_intermediate_vertices=max_intermediate_vertices,
+                            network_callbacks=network_callbacks,
+                            log_every=log_every,
                             score_logger=score_logger)
