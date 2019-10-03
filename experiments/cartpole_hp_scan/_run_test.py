cb16a39f86f59c8067942f050b5977d5520cbb35
Brian Timar <timarbrian@gmail.com>
2019-10-03 12:08:04 -0700

    update run test

--- experiments/cartpole_hp_scan/_run_test.py
+++ experiments/cartpole_hp_scan/_run_test.py
@@ -10,6 +10,8 @@ if __name__ == "__main__":
     nproc = int(sys.argv[1])
     print(f"Now running a bunch of independent tasks in {nproc} processes...")
     args = [ None for __ in range(2 * nproc) ]
-    with multiprocessing.Pool(nproc) as p:
-        p.map(test_task, args)
+    p = multiprocessing.Pool(nproc)
+    p.map(test_task, args)
+    p.close()
+    p.join()
     print(f"Python script finished")
